3e9e0accfc4e9ffa62406ea2a330239f229e986b
JARPinto <andre_pinto_10@hotmail.com>
2022-09-06 21:52:18 +0100

    Plates working

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from crypt import methods
+from functools import reduce
+import re
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect, session
 from flask_session import Session
@@ -15,6 +17,15 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+RANKS = [
+    "OF-9 Gen", "OF-8 TGen", "OF-7 MGen", "OF-6 BGen",
+    "OF-5 Cor", "OF-4 TCor", "OF-3 Maj",
+    "OF-2 Cap", "OF-1 Ten", "OF-1 Alf", "OF-D Asp",
+    "OR-9 SMor", "OR-8 SCh", "OR-7 SAj", 
+    "OR-6 1Sarg", "OR-5 2Sarg",
+    "OR-5 Furr", "OR-5 2Furr",
+    "OR-4 CSec", "OR-3 CAdj", "OR-2 1Cb", "OR-2 2Cb", "OR-1 Sold",
+]
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -24,29 +35,38 @@ def get_db_connection():
 @app.route('/')
 @login_required
 def index():
-    conn = get_db_connection()
-    users = conn.execute('SELECT * FROM users').fetchall()
-    conn.close()
-    return render_template('index.html', users=users)
+    db = get_db_connection()
+    soldiers = db.execute('SELECT * FROM soldiers').fetchall()
+    db.close()
+    return render_template('index.html', soldiers=soldiers)
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     """Register user"""
     if request.method == "POST":
         # Get variables
-        name = request.form.get("username")
+        bim = request.form.get("bim")
         pwd = request.form.get("password")
         pwd_confirm = request.form.get("confirmation")
-        print(name)
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        rank = request.form.get("rank")
+        print(bim)
         print(pwd)
         print(pwd_confirm)
         # Check variables
-        if not name:
-            flash('Username is required!')
+        if not bim:
+            flash('BIM is required!')
         elif not pwd:
             flash('Password is required!')
         elif not pwd_confirm:
             flash('Confirmation password is required!')
+        elif not first_name:
+            flash('First name is required!')
+        elif not last_name:
+            flash('Last name is required!')
+        elif not rank:
+            flash('Rank is required!')
         else:
             # Hash password and save to db.
             pwd_hash = generate_password_hash(pwd, method='pbkdf2:sha256', salt_length=8)
@@ -55,17 +75,17 @@ def register():
             cursor = db.cursor()
             print("Successfully Connected to SQL")
             
-            cursor.execute("SELECT * FROM users WHERE username = ?", (name,))
+            cursor.execute("SELECT * FROM soldiers WHERE bim = ?", (bim,))
             rows = cursor.fetchall()            
             
             if len(rows) == 0:
                 try:
-                    cursor.execute('INSERT INTO users (username, hash) VALUES (?, ?)',
-                            (name, pwd_hash))
+                    cursor.execute('INSERT INTO soldiers (bim, hash, first_name, last_name, rank) VALUES (?, ?, ?, ?, ?)',
+                            (bim, pwd_hash, first_name, last_name, rank))
                     db.commit()
                     print("Record inserted successfully", cursor.rowcount)
                     cursor.close()
-                    session["username"] = name
+                    session["user_id"] = bim
                 except sqlite3.Error as error:
                     print("Failed to insert data", error)
                 finally:
@@ -74,32 +94,32 @@ def register():
                         print("The db connection is closed")
                 return redirect(url_for('index'))
             else:
-                flash("Username already exists")
+                flash("BIM already exists")
                 
-    return render_template("register.html")
+    return render_template("register.html", ranks = RANKS)
         
 @app.route ("/login", methods=["POST", "GET"])
 def login():
     """Log user in"""
-    # Forget any username
+    # Forget any BIM
     session.clear()
 
     # if form is submite
     if request.method == "POST":
-        name = request.form.get("username")
+        bim = request.form.get("bim")
         pwd = request.form.get("password")
-        print("Login c/: " + name)
+        print("Login c/: " + bim)
         print("Pass login: " + pwd)
         # Check variables
-        if not name:
-            flash('Username is required!')
+        if not bim:
+            flash('BIM is required!')
         elif not pwd:
             flash('Password is required!')
         else:
-            # Query database for username
+            # Query database for BIM
             db = get_db_connection()
             cursor = db.cursor()
-            cursor.execute("SELECT * FROM users WHERE username = ?", (name,))
+            cursor.execute("SELECT * FROM soldiers WHERE bim = ?", (bim,))
             rows = cursor.fetchall()
             print("Database reading done")
             print(len(rows))
@@ -107,15 +127,15 @@ def login():
             if db:
                 db.close()
 
-            # Ensures username and password is correct
+            # Ensures BIM and password is correct
             if len(rows) != 1 or not check_password_hash(rows[0]["hash"], pwd):
-                flash("Invalid username and/or password")
+                flash("Invalid BIM and/or password")
 
             else:
-                # Remember username exists and password is correct
-                session["username"] = rows[0]["username"]
-                print("username and password correct")
-                print(rows[0]["username"])
+                # Remember BIM exists and password is correct
+                session["user_id"] = rows[0]["id"]
+                print("BIM and password correct")
+                print(rows[0]["id"])
                 
                 # Redirect user to home page 
                 return redirect("/")
@@ -125,4 +145,36 @@ def login():
 def logout():
     session.clear()
     #return redirect("/")
-    return redirect('/')
+    print("Fez logout")
+    # In the end -- render redirect('/')
+    return render_template("/logout.html")
+
+@app.route('/transports', methods=["POST", "GET"])
+@login_required
+def transports():
+    ''' Transports log '''
+    outros = ('abc', 'dcf', 'dgg', 'ggreg')
+    # Query database for fleet
+    db = get_db_connection()
+    plates = db.execute("SELECT * FROM fleet").fetchall()
+
+    for plate in plates:
+        print(plate)
+
+
+    if request.method == "POST":
+        user_id = session["user_id"]
+        # Get template data
+        plate = request.form.get("plate")
+        km_init = request.form.get("km_init")
+        km_final = request.form.get("km_final")
+        gas = request.form.get("gas")
+        
+        print(plate)
+        print(km_init)
+        print(km_final)
+        print(gas)
+
+        return redirect('/')
+
+    return render_template("transports.html", plates = plates)
